dd54fa9cc26ed27e02ee0b9861d5adf2f09f0621
alexander <viruses.list.ru@gmail.com>
2024-02-12 10:04:38 +0200

    initial commit

---
+++ ssqatest/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+from selenium import webdriver
+
+
+@pytest.fixture(scope="class")
+def init_driver(request):
+
+    pass
+    supported_browsers = ['chrome', 'firefox']
+
+    browser = os.environ.get('BROWSER')
+    if not browser:
+        raise Exception("The env variable BROWSER is not set")
+
+    browser = browser.lower()
+    if browser not in supported_browsers:
+        raise Exception(f"{browser} is not supported")
+
+    if browser in 'chrome':
+        driver = webdriver.Chrome()
+    elif browser in 'firefox':
+        driver = webdriver.Firefox()
+
+    request.cls.driver = driver
+    yield
+    # driver.quit()
---
+++ ssqatest/tests/test_dummy.py
@@ -0,0 +1,10 @@
+import pytest
+
+
+@pytest.mark.usefixtures("init_driver")
+
+class TestDummy():
+    def test_dummy_function(self):
+        print("Testing dummy function")
+        self.driver.get("http://google.com")
+        import pdb; pdb.set_trace()
